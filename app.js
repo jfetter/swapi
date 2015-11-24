0d03c8ff6191ced4bb949a0a70e0ed97512bd51b
Jillian Fetter <jillian.fetter@gmail.com>
2015-11-24 07:52:01 -0800

    nested maps able to assign id and grab character url

--- app.js
+++ app.js
@@ -2,30 +2,45 @@
 
 var app =  angular.module("swapiApp", ["ui.router"])
 
-app.constant("planets", []);
-app.constant("characters", []);
+
 
 // and one controller to control them all
-app.controller("PlanetsController",  function( $scope, PlanetService, planets, characters){
+app.controller("PlanetsController",  function( $scope, PlanetService){
 	console.log("INSIDE CONTROLLER")
 	$scope.getPlanets = function(){
 	PlanetService.getPlanets()
 	.then(function(res){
-			$scope.planets = res.data.results;
-			planets.push(res.data.results);
-			console.log(planets)
-		})
-	}
+ 		$scope.planets = res.data.results.map( planet => {
+ 			console.log(planet.residents);
+      planet.residents = planet.residents.map(resident => {
+          var resident = { url: resident };
+          resident.id = resident.url.match(/\d+/)[0];
+          console.log("RES", planet.residents, resident)
+          return resident;
+      });
+      PlanetService.planets.push(planet);
+      return planet;
+   		});
+ 			console.log("SCOPED PLANETS", $scope.planet.residents)
+			console.log("SERVICED PLANETS", PlanetService.planets)
+    }).catch(error => console.error(error.status));
+
+		}
 	$scope.getPlanets();
 
 	$scope.viewed = function(){
-		if (characters.indexOf($scope.resident.id)) {
-		return true
-		} else {
-			return false
-		 }
+		PlanetsService.characters.push($scope.this.id)
+		$scope.class = viewed; 
+		console.log(characters)
 	}
 });
+
+app.controller("ResidentCtrl", function($scope, $stateParams) {
+    $http.get("http://swapi.co/api/people/" + $stateParams.id + "/?format=json").then(resp => {
+        $scope.character = resp.data;
+    });
+})
+
 
 // configuration block: executed during provider configuration phase
 // only providers and constants can be injected into configuration --
@@ -35,23 +50,27 @@ app.config(['$urlRouterProvider', "$stateProvider", function($urlRouterProvider,
 	
 	$stateProvider
 		.state("planets", {url: "/planets", templateUrl: "partials/planets.html", controller: "PlanetsController"})
-
+		.state("character", {url: "/character/:id", templateUrl: "partials/character.html", controller: "CharacterController"})
 	$urlRouterProvider.otherwise("/planets");
 }]);
 
 app.service("PlanetService", function($http){
 	this.planets = [];
+	this.characters = [];
 	this.planet = {};
+	var url = "http://swapi.co/api/planets/?format=json"
 	this.getPlanets = function(){
 		return $http({
-			url: "http://swapi.co/api/planets/?format=json",
+			url: url,
 			method: "GET"
 		})
-		// 	.then(function(res){
-		// 	var planet = ({name: res.data.results.name, population: res.data.results.population})
-		// 	var planets = []
-		// 	planets.push(planet);
-		// 	console.log(planets);
-		// })		
 	};
+
+this.getCharacters = function(){
+	return $http({
+		url: url, 
+		method: "GET"
+	})
+}
+
 });
